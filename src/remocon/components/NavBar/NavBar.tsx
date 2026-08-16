4f03d02da14ed9ff4125aa5f1dc41ad80b32e403
Daniel Chen <daniel@ccns.ncku.edu.tw>
2026-08-15 18:20:29 -0700

    feat: show back navigation across remote pages

--- src/remocon/components/NavBar/NavBar.tsx
+++ src/remocon/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 // tslint:disable-next-line:no-submodule-imports
-import { FaHistory, FaHome, FaUserEdit } from "react-icons/fa";
-import { Link } from "react-router";
+import { FaArrowLeft, FaHistory, FaHome, FaUserEdit } from "react-icons/fa";
+import { Link, useLocation, useNavigate } from "react-router";
 // tslint:disable-next-line:no-submodule-imports no-implicit-dependencies
 import icon from "url:../../images/icon.png";
 
@@ -14,15 +14,40 @@ const NavBar = ({
   nickname: string;
   onResetNickname: () => void;
 }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isHome = location.pathname === "/";
+
+  const goBack = () => {
+    if (location.key === "default") {
+      navigate("/", { replace: true });
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
     <div className={styles.navBar}>
-      <Link to="/">
-        <FaHome />
-      </Link>
+      <div className={styles.actions}>
+        {!isHome && (
+          <button
+            className={styles.iconButton}
+            type="button"
+            onClick={goBack}
+            aria-label="Go back"
+            title="Back"
+          >
+            <FaArrowLeft />
+          </button>
+        )}
+        <Link to="/" aria-label="Home" title="Home">
+          <FaHome />
+        </Link>
+      </div>
       <img height={40} src={icon} alt="空" />
       <div className={styles.actions}>
         <button
-          className={styles.nicknameButton}
+          className={styles.iconButton}
           type="button"
           onClick={onResetNickname}
           aria-label={`Change nickname. Current nickname: ${nickname}`}
--- src/remocon/pages/SongPage.tsx
+++ src/remocon/pages/SongPage.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
-import { Link, useNavigate, useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
-import Button from "../components/Button";
 import DamQueueButtons from "../components/DamQueueButtons";
 import { withLoader } from "../components/Loader";
 import { SongPageQuery } from "./__generated__/SongPageQuery.graphql";
@@ -28,7 +27,6 @@ type RouteParams = {
 };
 
 const SongPage = () => {
-  const navigate = useNavigate();
   const params = useParams<RouteParams>();
   const data = useLazyLoadQuery<SongPageQuery>(songPageQuery, {
     id: params.id!,
@@ -37,9 +35,6 @@ const SongPage = () => {
 
   return (
     <div>
-      <Button type="button" onClick={() => navigate(-1)}>
-        ← Back to search results
-      </Button>
       <h2>{song.name}</h2>
       <Link to={`/search/artist/${song.artistName}`}>{song.artistName}</Link>
       {!!song.tieUp && <span> • {song.tieUp}</span>}
